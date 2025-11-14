83ef5bb0d3e3544ebf1512fb05b07876c1fcc331
RamishJahra <ramishjahra@gmail.com>
2025-11-14 21:40:00 +0600

    Added Backend and Mysql

--- backend/Employee_Management_System-Login/src/Pages/Dashboard/Assests.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Assests = () => {
-  return (
-    <div>Assests</div>
-  )
-}
--- backend/Employee_Management_System-Login/src/Pages/Dashboard/Recruitment.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Recruitment = () => {
-  return (
-    <div>Recruitment</div>
-  )
-}
